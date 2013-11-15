f3f322e9f7ae3b4914aead5d0a38b41ea9470c8b
Vross <vross@vross.me>
2013-11-15 10:16:08 +0000

    Add builder which contains the moveTo, lineTo and closePath functions.

--- builder.js
+++ builder.js
@@ -1,6 +1,45 @@
-function MoveTo(coordinates) {}
-function LineTo(coordinates) {}
-function ClosePath(coordinates) {}
-module.exports.MoveTo= MoveTo;
-module.exports.LineTo= LineTo;
-module.exports.ClosePath = ClosePath;
+// Builder class describing operations on Mapnik-vector-tiles's geometries
+
+function Builder() {
+    this.tileRelativeGeometry = [];
+}
+
+Builder.prototype.moveTo = function moveTo(x, y) {
+    x = decode_sint32(x);
+    y = decode_sint32(y);
+    geom_size = this.tileRelativeGeometry.length;
+    if (!geom_size) {
+        previous = [0, 0];
+    } else {
+        previous = this.tileRelativeGeometry[geom_size - 1];
+    }
+    this.tileRelativeGeometry.push([previous[0] + x, previous[1] + y]);
+};
+
+Builder.prototype.lineTo = function lineTo(x, y) {
+    x = decode_sint32(x);
+    y = decode_sint32(y);
+    geom_size = this.tileRelativeGeometry.length;
+    if (!geom_size) {
+        this.tileRelativeGeometry.push([0, 0]);
+        geom_size ++;
+    }
+    previous = this.tileRelativeGeometry[geom_size - 1];
+    this.tileRelativeGeometry.push([previous[0] + x, previous[1] + y]);
+};
+
+Builder.prototype.closePath = function closePath() {};
+
+var decode_sint32 = function (encoded_value) { // decode zigzag encoding
+    if (encoded_value % 2 == 0) {
+        return encoded_value / 2;
+    } else {
+        return - (encoded_value + 1) / 2;
+    }
+};
+
+Builder.prototype.getTileRelativeGeometry = function getTileRelativeGeometry() {
+    return this.tileRelativeGeometry;
+};
+
+module.exports.Builder = Builder;
---
+++ tests/test_builder.js
@@ -0,0 +1,31 @@
+var assert = require('assert');
+var builder = require('../builder.js');
+var director = require('../director.js');
+
+describe("Builder\n", function() {
+    beforeEach(function() {
+        myGeometry = new builder.Builder();
+    });
+
+    it("        MoveTo(40, 32) should give [20, 16]", function () {
+        myGeometry.moveTo(40, 32);
+        assert.deepEqual(myGeometry.getTileRelativeGeometry(), [[20, 16]])
+    });
+
+    it("        LineTo(2, 4) should give [0, 0, 1, 2]", function () {
+        myGeometry.lineTo(2, 4);
+        assert.deepEqual(myGeometry.getTileRelativeGeometry(), [[0, 0], [1, 2]]);
+    });
+
+    it("        MoveTo(6, 2), LineTo(1, 4) should give [6, 2, 1, 4]", function () {
+        myGeometry.moveTo(6, 2);
+        myGeometry.lineTo(1, 4);
+        assert.deepEqual(myGeometry.getTileRelativeGeometry(), [[3, 1], [2, 3]]);
+    });
+
+    it("        should works when called from the director", function () {
+        encoded = new director.Director();
+        relative_geometry = encoded.createDecodedTile([9, 30, 0]);
+        assert.deepEqual(relative_geometry, [[15, 0]]);
+    });
+})
