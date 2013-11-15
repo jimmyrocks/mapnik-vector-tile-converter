16deb6a2a9514828cb8ea1321643a4b9b64b57bc
Vross <vross@vross.me>
2013-11-15 10:17:15 +0000

    Change Director to be a class.

--- director.js
+++ director.js
@@ -1,5 +1,48 @@
 var builder = require('./builder.js');
 
+function Director() {
+    this.mapnikEncodedGeometry;
+};
+
+Director.prototype.setMapnikEncodedGeometry = function setMapnikEncodedGeometry(encoded) {
+    this.mapnikEncodedGeometry = encoded;
+};
+
+Director.prototype.callBuilderFunctions = function callBuilderFunctions(mapnikvtGeometry) {
+    geometry = this.mapnikEncodedGeometry;
+    var i = 0;
+    while (i<geometry.length) {
+        var command = get_command(geometry[i]);
+        var frequence = get_frequence(geometry[i]);
+        var next_i;
+        switch (command) {
+            case 1: // MoveTo
+                //TODO: call_f(i, next_i, geometry, frequence, builder.MoveTo);
+                next_i = i + 2 * frequence + 1;
+                coordinates = geometry.slice(i+1, next_i);
+                for (j = 0; j<coordinates.length ; j+=2) {
+                    mapnikvtGeometry.moveTo(coordinates[j], coordinates[j+1]);
+                }
+                i = next_i;
+                break;
+            case 2: // LineTo
+                next_i = i + 2 * frequence + 1;
+                coordinates = geometry.slice(i+1, next_i);
+                for (j = 0; j<coordinates.length ; j+=2) {
+                    mapnikvtGeometry.lineTo(coordinates[j], coordinates[j+1]);
+                }
+                i = next_i;
+                break;
+            case 7: //ClosePath
+                mapnikvtGeometry.closePath();
+                i++;
+                break;
+            default:
+                throw('Error while parsing PBF: invalid Mapnik-vector-tile geometry');
+        }
+    }
+}
+
 get_command = function (instruction) {
     var command = instruction & 7;
     return command;
@@ -10,31 +53,11 @@ get_frequence = function (instruction) {
     return frequence;
 }
 
-function construct(geometry) {
-    var i = 0;
-    while (i<geometry.length) {
-        var command = get_command(geometry[i]);
-        var frequence = get_frequence(geometry[i]);
-        switch (command) {
-            case 1: // MoveTo
-                x = geometry.slice(i+1)[0];
-                y = geometry.slice(i+1)[1];
-                builder.MoveTo(x, y);
-                i += 2 * frequence + 1;
-                break;
-            case 2: // LineTo
-                coordinates = geometry.slice(i+1);
-                builder.LineTo.apply(this, coordinates);
-                i += 2 * frequence + 1;
-                break;
-            case 7: //ClosePath
-                builder.ClosePath();
-                i++;
-                break;
-            default:
-                throw('Error while parsing PBF: invalid geometry');
-        }
-    }
+Director.prototype.createDecodedTile = function createDecodedTile(geometry) {
+    this.mapnikEncodedGeometry = geometry;
+    var relativeTileGeometryBuilder = new builder.Builder();
+    this.callBuilderFunctions(relativeTileGeometryBuilder);
+    return relativeTileGeometryBuilder.getTileRelativeGeometry();
 }
 
-module.exports.construct = construct;
+module.exports.Director = Director;
---
+++ tests/test_director.js
@@ -0,0 +1,66 @@
+var assert = require('assert');
+var director = require('../director.js');
+var builder = require('../builder.js');
+var sinon = require('sinon');
+
+
+describe("Director\n", function() {
+    before(function() {
+        encoded = new director.Director();
+    });
+
+    beforeEach(function() {
+        myGeometry = new builder.Builder();
+        move = sinon.spy(builder.Builder.prototype, "moveTo");
+        line = sinon.spy(builder.Builder.prototype, "lineTo");
+        close = sinon.spy(builder.Builder.prototype, "closePath");
+    });
+
+    afterEach(function() {
+        builder.Builder.prototype.moveTo.restore();
+        builder.Builder.prototype.lineTo.restore();
+        builder.Builder.prototype.closePath.restore();
+    });
+
+    it("        should call moveTo with parameters 12, 26 when [9, 12, 26]", function () {
+        instruction = [9, 12, 26];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.calledWithExactly(move, 12, 26);
+    });
+
+    it("        should call LineTo with parameters 28, 94 when [10, 28, 94]", function() {
+        instruction = [10, 28, 94];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.calledWithExactly(line, 28, 94);
+    });
+
+    it("        should call LineTo twice with parameters 42, 54 and 79, 63 when [18, 42, 54 79, 63]", function() {
+        instruction = [18, 42, 54, 79, 63];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.calledTwice(line);
+        sinon.assert.calledWithExactly(line, 42, 54);
+        sinon.assert.calledWithExactly(line, 79, 63);
+    });
+    it("        should call MoveTo twice with parameters 42, 54 and 79, 63 when [18, 42, 54 79, 63]", function() {
+        instruction = [17, 42, 54, 79, 63];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.calledTwice(move);
+        sinon.assert.calledWithExactly(move, 42, 54);
+        sinon.assert.calledWithExactly(move, 79, 63);
+    });
+    it("        should call MoveTo then LineTo when [9, 85, 69, 10, 90, 30]", function() {
+        instruction = [9, 85, 69, 10, 90, 30];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.callOrder(move, line);
+    });
+    it("        should call LineTo twice when [10, 85, 69, 10, 90, 30]", function() {
+        instruction = [10, 85, 69, 10, 90, 30];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.calledTwice(line);
+    });
+    it("        should call MoveTo then LineTo then ClosePath when [9, 85, 69, 10, 90, 30, 15]", function() {
+        instruction = [9, 85, 69, 10, 90, 30, 15];
+        encoded.createDecodedTile(instruction);
+        sinon.assert.callOrder(move, line, close);
+    });
+})
