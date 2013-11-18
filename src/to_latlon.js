57e1d73481e645310c30dc812d35c3c208850ce0
Vross <vross@vross.me>
2013-11-18 11:02:42 +0000

    Correct latlon values.

--- src/to_latlon.js
+++ src/to_latlon.js
@@ -1,23 +1,25 @@
-var delta = 4.948871220193411; //TODO remove hardcoded delta due to "y + 1" -> the NW corner of the tile under this one...
-
 function sinh(x){
     return (Math.exp(x) - Math.exp(-x)) / 2;
 }
 
-function tile_to_SW_latlon_deg(z, x, y) {
-    n = Math.pow(2, z);
-    lon = x / n * 360 - 180;
-    lat = Math.atan(sinh(Math.PI * (1 - 2 * (y + 1) / n))) * 180/Math.PI - delta;
-    return[lon, lat];
+function to_lon(n, xtile, x) {
+    lon = 360 * (xtile + x) / n - 180;
+    return lon;
+}
+
+function to_lat(n, ytile, y) {
+    lat_rad = Math.atan(sinh(Math.PI * (1 - 2 * (ytile + y) / n)));
+    lat = lat_rad * 180 / Math.PI;
+    return lat;
 }
 
 function convert_geometry_to_latlon(z, x, y, tile_geometry, extent) {
     var leaflet_geometry = [];
     for (var i=0; i<tile_geometry.length; i++) {
-        tile_SW_coord = tile_to_SW_latlon_deg(z, x, y);
-        ll_x = tile_SW_coord[0] + tile_geometry[i][0]*360/extent;
-        ll_y = tile_SW_coord[1] + tile_geometry[i][1]*180/extent;
-        leaflet_geometry.push([ll_x, ll_y]);
+        n = 1 << z; // 2^z
+        lon = to_lon(n, x, tile_geometry[i][0]/extent);
+        lat = to_lat(n, y, tile_geometry[i][1]/extent);
+        leaflet_geometry.push([lon, lat]);
     }
     if (leaflet_geometry.length == 1) {
         leaflet_geometry = leaflet_geometry[0];
--- tests/test_to_latlon.js
+++ tests/test_to_latlon.js
@@ -7,23 +7,42 @@ var to_latlon = require('../src/to_latlon.js');
 describe("Leaflet-vector-tile\n", function() {
     before(function() {
         encoded = new director.Director();
+        maxLat = 85.05112877980659;
     });
 
     it("        should convert coordinates for z=0, x=0, y=0, extent=4096 to a Leaflet geometry", function() {
         z = 0, x = 0, y = 0;
-        geometry = ([9, 0, 0]); //point at the SW corner of the tile
+        geometry = ([9, 0, 8192]); //point at the SW corner of the tile
         extent = 4096;
         tile_geometry = encoded.createDecodedTile(geometry);
         result = to_latlon.convert_geometry_to_latlon(z, x, y, tile_geometry, extent);
-        assert.deepEqual(result, [-180, -90]); //verify the y value
+        assert.deepEqual(result, [-180, -maxLat]); //verify the y value
     })
 
-    it("        should convert coordinates for z=0, x=0, y=0, extent=4096 to a Leaflet geometry", function() {
-        z = 0, x = 0, y = 0;
-        geometry = [9, 4096, 4096];
+    it("        should convert coordinates for z=1, x=0, y=1, extent=4096 to a Leaflet geometry", function() {
+        z = 1, x = 0, y = 1;
+        geometry = [9, 0, 8192];
         extent = 4096;
         tile_geometry = encoded.createDecodedTile(geometry);
         result = to_latlon.convert_geometry_to_latlon(z, x, y, tile_geometry, extent);
-        assert.deepEqual(result, [0, 0]);
+        assert.deepEqual(result, [-180, -maxLat]);
+    })
+
+    it("        should convert coordinates for z=2, x=0, y=3, extent=4096 to a Leaflet geometry", function() {
+        z = 2, x = 0, y = 3;
+        geometry = [9, 0, 8192];
+        extent = 4096;
+        tile_geometry = encoded.createDecodedTile(geometry);
+        result = to_latlon.convert_geometry_to_latlon(z, x, y, tile_geometry, extent);
+        assert.deepEqual(result, [-180, -maxLat]);
+    })
+
+    it("        should convert coordinates for z=2, x=3, y=3, extent=4096 to a Leaflet geometry", function() {
+        z = 2, x = 3, y = 3;
+        geometry = [9, 8192, 8192];
+        extent = 4096;
+        tile_geometry = encoded.createDecodedTile(geometry);
+        result = to_latlon.convert_geometry_to_latlon(z, x, y, tile_geometry, extent);
+        assert.deepEqual(result, [180, -maxLat]);
     })
 });
