0c69adbce988b7770aad755926dc673b0346a44a
Jim McAndrew <jim@loc8.us>
2014-01-14 16:47:06 -0700

    Revert tile.js to original

--- src/tile.js
+++ src/tile.js
@@ -1,45 +1,31 @@
-var director = require('./director.js'),
-geojsonize = require('./togeojson.js');
-converter = require('./converter.js'),
-convertTile = function (tile, tilePoint, filter) {
-  var decoder = new director.Decoder(),
-  parsedTile = {};
-  tile.forEach(function(layer) {
-    if (!filter || filter.indexOf(layer.name)+1) {
-      layer.features.forEach(function(feature) {
-        /*jshint camelcase: false */
-        var output = {},
-        relative = decoder.decode(feature.geometry);
-        output.geometry = converter.Converter.tolatlon(tilePoint.z, tilePoint.x, tilePoint.y, relative, layer.extent);
-        output.tags = [];
+var director = require('./director.js');
+var converter = require('./converter.js');
 
-        for (var i = 1 ; i < feature.tags.length ; i+=2) {
-          var tag = layer.values[feature.tags[i]];
-          if ((tag.string_value || tag.int_value || tag.double_value || tag.float_value || tag.bool_value || tag.sint_value || tag.uint_value) == null) {
-            tag = null;
-          } else if (tag.string_value) {
-            tag = tag.string_value;
-          } else {
-            tag = tag.int_value | tag.double_value | tag.float_value | tag.bool_value | tag.sint_value | tag.uint_value ;
-          }
-          output.tags.push({key: layer.keys[feature.tags[i-1]], value: tag});
-        }
-        output = geojsonize(output, feature.type, feature.id.low, relative, tilePoint, layer.extent);
-        output.properties.layer = layer.name;
-
-//        if (parsedTile[layer.name]) {
-        if (parsedTile['features']) {
-          //parsedTile[layer.name].push(output);
-          parsedTile['features'].push(output);
-        } else {
-          //parsedTile[layer.name] = [output];
-          parsedTile['features'] = [output];
-        }
-      });
-    }
-  });
-  parsedTile.type = "FeatureCollection";
-  return parsedTile;
-};
+var convertTile = function (tile, tilePoint) {
+    decoder = new director.Decoder();
+    tile.forEach(function(layer) {
+        layer.features.forEach(function(feature) {
+            relative = decoder.decode(feature.geometry);
+            feature.geometry = converter.Converter.tolatlon(tilePoint.z, tilePoint.x, tilePoint.y, relative, layer.extent);
+        });
+        var tags = []
+        layer.features.forEach(function(feature) {
+            mapnikTags = feature.tags;
+            feature.tags = [];
+            for (var i = 1 ; i < mapnikTags.length ; i+=2) {
+                tag = layer.values[mapnikTags[i]];
+                if ((tag.string_value || tag.int_value || tag.double_value || tag.float_value || tag.bool_value || tag.sint_value || tag.uint_value) == null) {
+                    tag = null;
+                } else if (tag.string_value) {
+                    tag = tag.string_value;
+                } else {
+                    tag = tag.int_value | tag.double_value | tag.float_value | tag.bool_value | tag.sint_value | tag.uint_value ;
+                }
+                feature.tags.push({key: layer.keys[mapnikTags[i-1]], value: tag});
+            }
+        });
+    });
+    return tile;
+}
 
 module.exports.convertTile = convertTile;
