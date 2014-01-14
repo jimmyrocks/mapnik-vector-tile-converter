2fb7cbba7612b1c2c6953e3ca623f9e41b19a8d0
Jim McAndrew <jim@loc8.us>
2014-01-14 16:46:28 -0700

    Clean up geojson code

---
+++ src/geojsonTile.js
@@ -0,0 +1,21 @@
+var geojsonize = require('./togeojson.js'),
+convertTile = function (tile, tilePoint, filter) {
+  var parsedTile = {};
+  tile.forEach(function(layer) {
+    if (!filter || filter.indexOf(layer.name)+1) {
+      layer.features.forEach(function(feature) {
+       var output = geojsonize(feature, tilePoint, layer);
+
+        if (parsedTile.features) {
+          parsedTile.features.push(output);
+        } else {
+          parsedTile.features = [output];
+        }
+      });
+    }
+  });
+  parsedTile.type = 'FeatureCollection';
+  return parsedTile;
+};
+
+module.exports.convertTile = convertTile;
--- src/togeojson.js
+++ src/togeojson.js
@@ -1,29 +1,55 @@
-var converter = require('./converter');
-var toLonLat = function (geometries, tilePoint, extent, type) {
-  var newGeometries = [[]];
-  var polygonIndex = 0;
-  var cleanLast = function(idx) {
-      // Don't think we need to do this
-      //newGeometries[idx].pop();
-      newGeometries[idx].push(newGeometries[idx][0]);
-  }
- 
+var director  = require('./director.js'),
+converter = require('./converter'),
+toLonLat = function (geometries, tilePoint, extent, type) {
+  var newGeometries = [[]],
+  polygonIndex = 0,
+  closePolygon = function(idx) {
+    // Don't think we need to do this
+    //newGeometries[idx].pop();
+    newGeometries[idx].push(newGeometries[idx][0]);
+  };
+
   for (var i= 0; i<geometries.length; i++) {
     if (geometries[i][2] >= newGeometries.length) {
-      cleanLast(polygonIndex)
+      closePolygon(polygonIndex);
       polygonIndex = newGeometries.push([]) - 1;
     } else {
       var newGeos = converter.Converter.tolatlon(tilePoint.z, tilePoint.x, tilePoint.y, [geometries[i]], extent)[0];
       newGeometries[polygonIndex].push([newGeos[1], newGeos[0]]);
     }
   }
-  if (type === 3) {
-    cleanLast(polygonIndex);
+  var returnTypes = {
+    '1': function() {
+      return newGeometries[0][0];
+    },
+    '2': function() {
+      return newGeometries[0];
+    },
+    '3': function() {
+      closePolygon(polygonIndex);
+      return newGeometries;
+    }
+  };
+  return returnTypes[type]();
+},
+parseTags = function(rawTags, layer) {
+  /*jshint camelcase: false */
+  var tags = [];
+  for (var i = 1 ; i < rawTags.length ; i+=2) {
+    var tag = layer.values[rawTags[i]];
+    if ((tag.string_value || tag.int_value || tag.double_value || tag.float_value || tag.bool_value || tag.sint_value || tag.uint_value) == null) {
+      tag = null;
+    } else if (tag.string_value) {
+      tag = tag.string_value;
+    } else {
+      tag = tag.int_value | tag.double_value | tag.float_value | tag.bool_value | tag.sint_value | tag.uint_value ;
+    }
+    tags.push({key: layer.keys[rawTags[i-1]], value: tag});
   }
-  return newGeometries;
+  return tags;
 };
 
-exports = module.exports = function(entry, type, id, relative, tilePoint, extent) {
+exports = module.exports = function(feature, tilePoint, layer) {
   var newEntry = {
     'type': 'Feature',
     'geometry': {
@@ -32,26 +58,45 @@ exports = module.exports = function(entry, type, id, relative, tilePoint, extent
     },
     'properties': {
     },
-    'id': id
+    'id': feature.id.low
+  },
+  decoder = new director.Decoder(),
+  tags = parseTags(feature.tags, layer);
+
+  // Tags
+  
+  for (var i = 0; i < tags.length; i++) {
+    newEntry.properties[tags[i].key] = tags[i].value;
+  }
+  // Add the layername to the tags
+  tags.layer = layer.name;
+
+  // Geometry
+  var relative = decoder.decode(feature.geometry);
+
+  var types = {
+    '1': {
+      'type': 'Point',
+      'coordinates': function() {
+        return toLonLat(relative, tilePoint, layer.extent, 1);
+      }
+    },
+    '2': {
+      'type': 'LineString',
+      'coordinates': function() {
+        return toLonLat(relative, tilePoint, layer.extent, 2);
+      }
+    },
+    '3': {
+      'type': 'Polygon',
+      'coordinates': function() {
+        return toLonLat(relative, tilePoint, layer.extent, 3);
+      }
+    }
   };
 
-  for (var i = 0; i < entry.tags.length; i++) {
-    newEntry.properties[entry.tags[i].key] = entry.tags[i].value;
-  }
-
-  if (type === 1) {
-    // Point
-    newEntry.geometry.type = 'Point';
-    newEntry.geometry.coordinates = toLonLat(relative, tilePoint, extent)[0][0];
-  } else if (type === 2) {
-    // Line
-    newEntry.geometry.type = 'LineString';
-    newEntry.geometry.coordinates = toLonLat(relative, tilePoint, extent)[0];
-  } else if (type === 3) {
-    // Polygon
-    newEntry.geometry.type = 'Polygon';
-    newEntry.geometry.coordinates = toLonLat(relative, tilePoint, extent, type);
-  }
+  newEntry.geometry = types[feature.type];
+  newEntry.geometry.coordinates = newEntry.geometry.coordinates();
 
   return newEntry;
 };
