5276eb74ae340c54a2415626365bb4a43014d9f9
Jim McAndrew <jim@loc8.us>
2014-03-08 16:24:51 -0700

    comment

--- example/js/tileStream.js
+++ example/js/tileStream.js
@@ -1,31 +1,35 @@
-var mvt = require('../src/nodePBFreader.js'),
-tile = require('../src/geojsonTile.js'),
-http = require('http'),
-getUrl = function (url, callback) {
-  var chunks = [];
-  console.log('u', url);
-  http.get(url, function(res){
-    res.on('data', function(chunk){
-      chunks.push(chunk);
+var mvt = require('../../src/nodePBFreader.js'),
+  tile = require('../../src/geojsonTile.js'),
+  http = require('http'),
+  getUrl = function(url, callback) {
+    var chunks = [];
+    console.log('u', url);
+    http.get(url, function(res) {
+      res.on('data', function(chunk) {
+        chunks.push(chunk);
+      });
+      res.on('end', function() {
+        var buffer = new Buffer(chunks.reduce(function(prev, current) {
+          return prev.concat(Array.prototype.slice.call(current));
+        }, []));
+        callback(buffer);
+      });
     });
-    res.on('end', function() {
-      var buffer = new Buffer(chunks.reduce(function(prev, current) {
-        return prev.concat(Array.prototype.slice.call(current));
-      }, []));
-      callback(buffer);
-    });
-  });
-};
+  };
 
-exports = module.exports = function (zxy, callback) {
+exports = module.exports = function(zxy, callback) {
   // http://b.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4/14/4748/6227.vector.pbf
-  var keys = ['a','b','c','d'],
-  url = 'http://' + keys[[(parseInt(zxy[1],10) * 31 + parseInt(zxy[2],10)) % 4]] + '.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4/' + zxy.join('/') + '.vector.pbf';
+  var keys = ['a', 'b', 'c', 'd'],
+    url = 'http://' + keys[[(parseInt(zxy[1], 10) * 31 + parseInt(zxy[2], 10)) % 4]] + '.tiles.mapbox.com/v3/mapbox.mapbox-streets-v4/' + zxy.join('/') + '.vector.pbf';
 
 
   getUrl(url, function(chunks) {
     mvt.parse(chunks, function(data) {
-      callback(tile.convertTile(data, {'z': parseInt(zxy[0],10), 'x': parseInt(zxy[1],10), 'y': parseInt(zxy[2],10)}, ['water','building', 'road', 'bridge', 'tunnel', 'landuse']));
+      callback(tile.convertTile(data, {
+        'z': parseInt(zxy[0], 10),
+        'x': parseInt(zxy[1], 10),
+        'y': parseInt(zxy[2], 10)
+      }, ['water', 'building', 'road', 'bridge', 'tunnel', 'landuse']));
       //callback(tile.convertTile(data, {'z': '14', 'x': 4748, 'y': 6227}));
       //callback(chunks.length);
       //var md5 = crypto.createHash('md5');
