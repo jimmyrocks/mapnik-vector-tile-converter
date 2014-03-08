f9b2c6c2a28355a2a723acaa5baddc4dcb2d0b99
Jim McAndrew <jim@loc8.us>
2014-03-08 16:24:10 -0700

    comment

--- example/js/read.js
+++ example/js/read.js
@@ -1,34 +1,37 @@
-var mvt = require('../src/nodePBFreader.js'),
-crypto = require('crypto'),
-director = require('../src/director.js'),
-converter = require('../src/converter.js'),
-tile = require('../src/geojsonTile.js'),
-z = 14,
-x = 4748,
-y = 6227,
-decoder = new director.Decoder(),
-loadTile = function(data) {
-  var convertedTile = tile.convertTile(data, {'z': z, 'x': x, 'y': y});
-  console.log(JSON.stringify(convertedTile,null,2));
-},
-parsePbf = function(data) {
-  data.forEach(function(layer) {
-    // Loop through the layers
-    var features = layer.features,
-    //geometries = [],
-    extent = layer.extent;
-
-    // Loop through the individual features
-    features.forEach(function(feature) {
-      // Extract the geometries
-      var relative = decoder.decode(feature.geometry);
-      console.log(converter.Converter.tolatlon(z, x, y, relative, extent));
+var mvt = require('../../src/nodePBFreader.js'),
+  director = require('../../src/director.js'),
+  converter = require('../../src/converter.js'),
+  tile = require('../../src/geojsonTile.js'),
+  z = 14,
+  x = 4748,
+  y = 6227,
+  decoder = new director.Decoder(),
+  loadTile = function(data) {
+    var convertedTile = tile.convertTile(data, {
+      'z': z,
+      'x': x,
+      'y': y
     });
-  });
-};
+    console.log(JSON.stringify(convertedTile, null, 2));
+  },
+  parsePbf = function(data) {
+    data.forEach(function(layer) {
+      // Loop through the layers
+      var features = layer.features,
+        //geometries = [],
+        extent = layer.extent;
 
-var fs = require('fs'),
-fileName = './tiles/14_4748_6227.vector.pbf';
+      // Loop through the individual features
+      features.forEach(function(feature) {
+        // Extract the geometries
+        var relative = decoder.decode(feature.geometry);
+        console.log(converter.Converter.tolatlon(z, x, y, relative, extent));
+      });
+    });
+  },
+  fs = require('fs'),
+  fileName = '../tiles/14_4748_6227.vector.pbf';
+
 fs.readFile(fileName, function(err, contents) {
   var actionFunc = [parsePbf, loadTile];
   if (!err) {
